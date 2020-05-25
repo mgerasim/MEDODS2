fb3a2730c78199d0c1cfb0dc8d2eecea5b6c3f73
Mikhail Gerasimov <mgerasim@inbox.ru>
2020-05-25 21:08:02 +1000

    Подготовлен файл равертывания: описаны поэьапные шаги

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,18 @@ git commit -m deploy
 git pull origin master
 git push origin master
 sudo systemctl stop MEDODS.service
-sequelize db:migrate --env production
-sudo cp MEDODS.service /etc/systemd/system/
-sudo service systemd-resolved restart
-sudo systemctl enable MEDODS.service
-sudo systemctl start MEDODS.service
-sudo systemctl status MEDODS.service
 
-sudo mkdir ~/apps/MEDODS
 sudo cd ~/apps/MEDODS
-git clone git@github.com:mgerasim/MEDODSServer.git .
+
+sudo git pull origin master
+
 npm install
 npm install axios
 npm run build
+
+sequelize db:migrate --env production
+
+
+sudo systemctl start MEDODS.service
+
+
